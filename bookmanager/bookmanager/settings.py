6aa445569b3cb752a7ddd757a32a4df8042ea9f6
kenglong <kenglong1988@hotmail.com>
2022-11-07 19:19:06 +0800

    app config

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book',
+    #'book',
+    'book.apps.BookConfig'
+
 ]
 
 MIDDLEWARE = [
